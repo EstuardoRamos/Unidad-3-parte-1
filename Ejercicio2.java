f7834e335717a2960d260c73e88ec9fcbfe28df5
EstuardoRamos <estuardoramos201830358@cunoc.edu.gt>
2021-06-08 23:37:06 -0600

    Ejercicio2 java

---
+++ Ejercicio2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        String pais, capital;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese nombre del pais: ");
+        pais=scanner.nextLine();
+        System.out.println("Ingrese la capital: ");
+        capital=scanner.nextLine();
+        
+        System.out.println(capital+ " es la capital de "+pais);  
+    }
+    
+}
