0dbd4132ab5ea8e3412ec3db9a3e78f969dc7f00
EstuardoRamos <estuardoramos201830358@cunoc.edu.gt>
2021-06-09 01:39:50 -0600

    Ejercicio9 java

---
+++ Ejercicio9.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Ejercicio9 {
+    public static void main(String[] args) {
+        String a, b , A,B;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese la palabra A: ");
+        a=scanner.nextLine();
+        System.out.print("Ingrese la palabra B: ");
+        b=scanner.nextLine();
+        A=b;
+        B=a;
+        
+        System.out.println("Palbra A es "+A+" la palabra B es "+B);  
+    }
+    
+}
