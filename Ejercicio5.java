8ae90ba0ac382387d15249525e5fb951558ab54e
EstuardoRamos <estuardoramos201830358@cunoc.edu.gt>
2021-06-09 00:11:19 -0600

    Ejercicio5 java

---
+++ Ejercicio5.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        int num;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un numero: ");
+        num= scanner.nextInt();
+        num++;
+        System.out.println("El numero que sigue es "+num);
+        
+	}
+}
